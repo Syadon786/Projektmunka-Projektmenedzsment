94a3b818c2aa2b89ecad303a1348f4f85e9d80f8
Syadon <bokagabor4@gmail.com>
2022-10-15 23:10:15 +0200

    project tree page init

---
+++ src/pages/ProjectTree/ProjectTree.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Page from '../../components/Page/Page'
+
+const ProjectTree = () => {
+  return (
+    <Page title="Project tree" noCard></Page>
+  )
+}
+
+export default ProjectTree

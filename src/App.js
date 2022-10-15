b8f24f797c5e8bf6cfd90148a87766740db402ad
Syadon <bokagabor4@gmail.com>
2022-10-15 23:09:36 +0200

    tree route and css import

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import { BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-rou
 import HomePage from './pages/HomePage/HomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import Navbar from './components/Navbar/Navbar';
+import ProjectTree from './pages/ProjectTree/ProjectTree';
+
+import "./App.css";
 
 const App = () => {
   return (
     <Router>
-    <Navbar/>
     <div className="App">
-    <header className="App-header">
-      {/* <Navbar/> */}
-    </header>
-    <Routes>
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/notfound" element={<NotFoundPage />} />
+    <Navbar/>
+    <div className="content">
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/tree" element={<ProjectTree />} />
+        <Route path="/notfound" element={<NotFoundPage />} />
 
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="*" element={<Navigate to="/notfound" replace />} />
-    </Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<Navigate to="/notfound" replace />} />
+      </Routes>
+    </div>
   </div>
   </Router>
   )

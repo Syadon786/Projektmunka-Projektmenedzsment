2ef1ff08666d2b40ccb2339d5995d9aca1a26ba1
Syadon <bokagabor4@gmail.com>
2022-10-15 23:12:32 +0200

    menu navigation, user-info

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { faker } from "@faker-js/faker";
 
 import "./SideMenu.css";
 import logo from "../../assets/react-2.svg";
+import MenuItem from '../MenuItem/MenuItem';
 
 const SideMenu = () => {
-    const [active, setActive] = useState(true);
+    const [active, setActive] = useState(false);
+    const navigate = useNavigate();
 
     const SideMenuConfig = {
         inactive:  {
@@ -30,17 +34,43 @@ const SideMenu = () => {
                 <img src={logo} alt="logo"/>
             </div>
             <div className="toggle-menu-btn" onClick={toggleMenu}>
-                <i class={`bi bi-arrow-${SideMenuConfig[configIndex].toggleAlignment}-square-fill`}></i>
+                <i className={`bi bi-arrow-${SideMenuConfig[configIndex].toggleAlignment}-square-fill`}></i>
             </div>
         </div>
 
         <div className="search-controller">
             <button className="search-menu-btn">
-                <i class="bi bi-search"></i>
+                <i className="bi bi-search"></i>
             </button>
             <input type="text" placeholder="search"></input>         
         </div>
         <div className="divider"/>
+        <div className="main-menu">
+            <ul>
+                <MenuItem label="Dashboard" iconName="speedometer2" menuStatus={active} 
+                onClick={() => navigate("/home")}/>
+                <MenuItem label="Content" iconName="newspaper" menuStatus={active}>
+                    <MenuItem submenu label="Tree demo" iconName="bar-chart-steps" menuStatus={active}
+                    onClick={() => navigate("/tree")}
+                    />
+                    <MenuItem submenu menuStatus={active}
+                    onClick={() => alert("Just a dummy")}
+                    />
+                </MenuItem>
+                <MenuItem label="Not found test" iconName="bug" menuStatus={active} 
+                onClick={() => navigate("/notfound")}/>
+            </ul>
+        </div>
+       
+        <div className="side-menu-footer">
+            <div className="avatar">
+                <img src={faker.image.avatar()} alt="user profile-pic"/>            
+            </div>
+            {active ? <div className="user-info">
+                    <h5>{faker.name.fullName()}</h5>
+                    <p>{faker.internet.email()}</p>
+            </div> : null}                     
+        </div>
     </div>
     )
 }

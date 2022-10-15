6b5b3c0424c22c9684e3f08578f03474605ad323
Syadon <bokagabor4@gmail.com>
2022-10-15 17:48:13 +0200

    App component init, basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate} from "react-router-dom";
+
+import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+    <div className="App">
+    <header className="App-header">
+      {/* <Navbar/> */}
+    </header>
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/notfound" element={<NotFoundPage />} />
+
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="*" element={<Navigate to="/notfound" replace />} />
+    </Routes>
+  </div>
+  </Router>
   )
 }
 

e7eb7246861077430a666ddbce67abc45975c78b
Syadon <bokagabor4@gmail.com>
2022-10-16 01:34:15 +0200

    notfoundpage responsivity update

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -3,12 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 import Button from '../../components/Button/Button';
 
+import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
     
   return (
-  <div className="d-flex align-items-center justify-content-center vh-100">
+  <div className="container d-flex align-items-center justify-content-center vh-100">
       <div className="text-center">
           <h1 className="display-1 fw-bold">404</h1>
           <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>

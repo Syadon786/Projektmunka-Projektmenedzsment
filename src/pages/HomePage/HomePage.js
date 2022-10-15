93666460e75356b520535f8c66edbefb79a81ffb
Syadon <bokagabor4@gmail.com>
2022-10-16 00:35:30 +0200

    refactor classes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import Page from '../../components/Page/Page'
 const HomePage = () => {
   return (
     <Page title="Home" noCard>
-        <div className='container d-flex justify-content-center'>
+        <div className='d-flex justify-content-center'>
         <div className="card my-4  p-4 bg-white shadow text-center">
             Dummy content
         </div>

b0cbd1a429167a7ec48494986647db85b0e55fb2
Syadon <bokagabor4@gmail.com>
2022-10-15 17:46:32 +0200

    homepage init

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Page from '../../components/Page/Page'
+
+const HomePage = () => {
+  return (
+    <Page title="Home" noCard>
+        <div className='container d-flex justify-content-center'>
+        <div className="card my-4  p-4 bg-white shadow text-center">
+            Dummy content
+        </div>
+        </div>
+    </Page>
+  )
+}
+
+export default HomePage

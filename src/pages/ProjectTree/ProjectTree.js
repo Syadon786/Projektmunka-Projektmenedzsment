ea0827ac6ef7ba7ac74b97b99dc7d0e85bf648cb
Syadon <bokagabor4@gmail.com>
2022-10-16 23:05:39 +0200

    circular progress bar inside node demo

--- src/pages/ProjectTree/ProjectTree.js
+++ src/pages/ProjectTree/ProjectTree.js
@@ -3,10 +3,14 @@ import Page from '../../components/Page/Page'
 
 import  SortableTree from 'react-sortable-tree';
 import 'react-sortable-tree/style.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+import './ProjectTree.css';
 
 const ProjectTree = () => {
   const [treeData, setTreeData] = useState([
-    { title: 'Project', children: [
+    { title: 'Project', root: true, children: [
       { title: 'Frontend' , children: [{title : "UI"}]},
       { title: 'Backend' , children: [{title : "Authentication"}]}
     ] }
@@ -29,7 +33,13 @@ const ProjectTree = () => {
           canDrop={({nextPath}) => {if(nextPath.length === 1) 
                                     return false;
                                     return true;
-          }}                          
+          
+          }}                
+          generateNodeProps={(rowInfo) => ({
+            buttons: [
+              <button className="bar-btn"><CircularProgressbar value={0.66} maxValue={1} text="66%"/></button>,
+            ]
+          })}
         />
     </Page>
   )

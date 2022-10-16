8118f63776ba5bff0e004e57edefb98866cac308
Syadon <bokagabor4@gmail.com>
2022-10-16 11:13:47 +0200

    tree demo with dummy data

--- src/pages/ProjectTree/ProjectTree.js
+++ src/pages/ProjectTree/ProjectTree.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Page from '../../components/Page/Page'
 
+import  SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
+
 const ProjectTree = () => {
+  const [treeData, setTreeData] = useState([
+    { title: 'Project', children: [
+      { title: 'Frontend' , children: [{title : "UI"}]},
+      { title: 'Backend' , children: [{title : "Authentication"}]}
+    ] }
+  ])
+
+  useEffect(() => {
+    console.log(treeData);
+  });
+
   return (
-    <Page title="Project tree" noCard></Page>
+    <Page title="Project tree" noCard>
+        <SortableTree
+          treeData={treeData}
+          onChange={((treeData) => {setTreeData(treeData)})}
+          isVirtualized={false}
+        />
+    </Page>
   )
 }
 

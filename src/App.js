d20368705b87ea49e81cc6c71e58632756ed5913
Syadon <bokagabor4@gmail.com>
2022-10-27 14:34:48 +0200

    actProject state changed with Select

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 
 import HomePage from './pages/HomePage/HomePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import Navbar from './components/Navbar/Navbar';
 import ProjectTree from './pages/ProjectTree/ProjectTree';
 import LoginPage from './pages/LoginPage/LoginPage';
 
 import "./App.css";
+import SideMenu from './components/SideMenu/SideMenu';
 
 const App = () => {
+  const [actProject, setActProject] = useState({value: "Project", label: "Project"});
+  useEffect(() => {
+    console.log(actProject);
+  }, [actProject]) 
+
   return (
     <div className="App"> 
     <Router>
     <header className="App-header">
-        <Navbar/>
+        <SideMenu onProjectChange={(value) => {setActProject(value)
+        }}/>
     </header>
     <div className="content">
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<HomePage actProject={actProject}/>} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/tree" element={<ProjectTree />} />
+        <Route path="/tree" element={<ProjectTree actProject={actProject}/>} />
         <Route path="/notfound" element={<NotFoundPage />} />
 
         <Route path="/" element={<Navigate to="/home" replace />} />

1dd16d8568d38f4aebd1dbe925f98b6b86426182
Syadon <bokagabor4@gmail.com>
2022-11-01 00:59:28 +0100

    hash old and act state to detect is tree has been changed

--- src/pages/ProjectTree/ProjectTree.js
+++ src/pages/ProjectTree/ProjectTree.js
@@ -1,55 +1,73 @@
 import React, {useEffect, useState} from 'react'
 import Page from '../../components/Page/Page'
 
-import  SortableTree from 'react-sortable-tree';
+import  SortableTree, { toggleExpandedForAll } from 'react-sortable-tree';
 import 'react-sortable-tree/style.css';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+
+import Button from '../../components/Button/Button';
+
+import hash from 'object-hash'
 
 import './ProjectTree.css';
 import '../../styles/ProjectTree.css';
 
 const ProjectTree = ({actProject}) => {
   const [treeData, setTreeData] = useState([
-    { title: 'Project', root: true, children: [
-      { title: 'Frontend' , children: [{title : "UI"}]},
-      { title: 'Backend' , children: [{title : "Authentication"}]}
-    ] }
-  ])
+    { title: actProject.label, root: true,  children: [
+      { title: 'Frontend' , children: [{title : "UI", expanded: false}], expanded: false},
+      { title: 'Backend' , children: [{title : "Authentication", expanded: false,}], expanded: false}
+    ], expanded: false}
+  ]);
 
-  const demoDrawTreeDataState = (data) => {
-    return(
-      <ol>{
-         data.map(node => {
-            if(node.children)
-              return <li key={node.title}>{node.title}<ol>{demoDrawTreeDataState(node.children)}</ol></li>          
-            else return <li key={node.title}>{node.title}</li>            
-         })
-        }
-      </ol>
-    );
-  }
+  const [oldTreeData, setOldTreeData] = useState([
+    { title: actProject.label, root: true,  children: [
+      { title: 'Frontend' , children: [{title : "UI", expanded: false}], expanded: false},
+      { title: 'Backend' , children: [{title : "Authentication", expanded: false,}], expanded: false}
+    ], expanded: false}
+  ]);
+
+  const [oldStateHash, setOldStateHash] = useState("");
+  const [newStateHash, setNewStateHash] = useState("");
 
   useEffect(() => {
-    console.dir(treeData);
+    setNewStateHash(hash(treeData[0]));
+    console.dir(treeData[0])
   }, [treeData]);
 
   useEffect(() => {
+     setOldStateHash(hash(oldTreeData[0]));
+  }, [oldTreeData])
+
+  useEffect(() => {
     setTreeData([
-      { title: actProject.label, root: true, children: [
-        { title: 'Frontend' , children: [{title : "UI"}]},
-        { title: 'Backend' , children: [{title : "Authentication"}]}
-      ] }
+      { title: actProject.label, root: true,  children: [
+        { title: 'Frontend' , children: [{title : "UI", expanded: false}], expanded: false},
+        { title: 'Backend' , children: [{title : "Authentication", expanded: false,}], expanded: false}
+      ], expanded: false}
+    ]);
+
+    setOldTreeData([
+      { title: actProject.label, root: true,  children: [
+        { title: 'Frontend' , children: [{title : "UI", expanded: false}], expanded: false},
+        { title: 'Backend' , children: [{title : "Authentication", expanded: false,}], expanded: false}
+      ], expanded: false}
     ]);
   }, [actProject]);
 
-  
-
   return (
     <Page title={actProject.label} noCard>
+          {(oldStateHash !== newStateHash) ? 
+          <>
+          {/* <Button onClick={() => {setTreeData(toggleExpandedForAll({treeData: treeData, expanded: false}))}}>Collapse all</Button> */}
+          <Button onClick={() => setTreeData(oldTreeData)}>Cancel changes</Button>
+          <Button className="ms-2">Save changes</Button>
+          </>
+           : null}
         <SortableTree
           treeData={treeData}
-          onChange={((treeData) => {setTreeData(treeData)})}
+          onChange={((prevData) => {setTreeData(prevData)})}
           isVirtualized={false}
           canDrag={({treeIndex}) => {if(treeIndex !== 0) 
                                           return true;
@@ -66,9 +84,10 @@ const ProjectTree = ({actProject}) => {
             ]
           })}
         />
-        <div className="DEMO card mt-4 p-4">TreeData state: csak hogy monitorozni lehessen fejlesztés közben a TreeData objektum felépítését
-          {demoDrawTreeDataState(treeData)}
-        </div>
+
+        <div>Eredeti állapot: {oldStateHash}</div>
+        <div>Új állapot: {newStateHash}</div>
+
     </Page>
   )
 

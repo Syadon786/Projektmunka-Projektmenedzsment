7adee6073ee80adf81647180e23dc6a59d1bfeec
Syadon <bokagabor4@gmail.com>
2022-11-01 19:08:14 +0100

    wrap App to provide project data to all components through Context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AuthProvider from './contexts/AuthContext';
+import ProjectProvider from './contexts/ProjectContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthProvider>
-        <App/>
+        <ProjectProvider>
+            <App/>
+        </ProjectProvider>
     </AuthProvider>
 );

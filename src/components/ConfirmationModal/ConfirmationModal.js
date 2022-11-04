430c8b9fecbf7ea15225e7221dbb36e1dc73752e
Syadon <bokagabor4@gmail.com>
2022-11-04 19:28:44 +0100

    confirmation modal

---
+++ src/components/ConfirmationModal/ConfirmationModal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from '../Button/Button'
+
+const ConfirmationModal = ({id, title, children, btnColor="primary", approveText, approveFunc}) => {
+    return (
+        <>
+        <div className="modal fade" tabIndex="-1" id={id}>
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title">{title}</h5>
+                <Button color="light" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></Button>
+            </div>
+            <div className="modal-body">      
+               {children}
+            </div>              
+            <div className="modal-footer">
+                <Button color="secondary" data-bs-dismiss="modal">Close</Button>
+                <Button color={btnColor} data-bs-dismiss="modal" onClick={() => approveFunc()}>{approveText}</Button>
+            </div>
+            </div>
+            </div>
+        </div>
+        </>
+      )
+}
+
+export default ConfirmationModal

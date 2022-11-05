81bc5a257c85aae232500499bb3867673bcbb16d
Syadon <bokagabor4@gmail.com>
2022-11-05 23:27:25 +0100

    modals refactor

--- src/components/TaskModal/TaskModal.js
+++ src/components/TaskModal/TaskModal.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import Button from '../Button/Button'
 import  {addNodeUnderParent} from 'react-sortable-tree';
+import FormModal from '../FormModal/FormModal';
 
 const TaskModal = ({title, treeData, rowInfo, setTreeData}) => {
   const [taskName, setTaskName] = useState("");
@@ -17,46 +17,27 @@ const TaskModal = ({title, treeData, rowInfo, setTreeData}) => {
   }
 
   return (
-    <>
-    <div className="modal fade" tabIndex="-1" id="taskModal">
-    <div className="modal-dialog">
-        <div className="modal-content">
-        <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
-            <Button color="light" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></Button>
-        </div>
-        <form className="col-md-12 go-right">
-            <div className="modal-body">
-                <div className="form-group">
-                <label >Task name</label>
-                <input type="text" 
-                    value={taskName}
-                   onChange={(event) => setTaskName(event.target.value)} 
-                    className="form-control" required/>
-            </div>
-            <div className="form-group mt-2">
-                <label>Task description (in progress)</label>
-                <textarea className="form-control"></textarea>
-            </div>
-            <div className="form-group mt-2">
-                <label>Add members to the task(in progress)</label>
-                <textarea className="form-control"></textarea>
-            </div>
-
-            </div>
-            <div className="modal-footer">
-                <Button onClick={(event) => event.preventDefault()} color="secondary" data-bs-dismiss="modal">Close</Button>
-                <Button data-bs-dismiss="modal" onClick={(event) => {
-                    event.preventDefault()
+    <FormModal id="taskModal" title={title} approveText="Save" 
+    approveFunc={() => {  
                     setTreeData(addChildNode());
                     setTaskName("");
-                }}>Save</Button>
-            </div>
-        </form>
-        </div>
-    </div>
-    </div>
-    </>
+      }}>
+      <div className="form-group">
+          <label >Task name</label>
+          <input type="text" 
+              value={taskName}
+              onChange={(event) => setTaskName(event.target.value)} 
+              className="form-control" required/>
+      </div>
+      <div className="form-group mt-2">
+          <label>Task description (in progress)</label>
+          <textarea className="form-control"></textarea>
+      </div>
+      <div className="form-group mt-2">
+          <label>Add members to the task(in progress)</label>
+          <textarea className="form-control"></textarea>
+      </div>
+    </FormModal>
   )
 }
 

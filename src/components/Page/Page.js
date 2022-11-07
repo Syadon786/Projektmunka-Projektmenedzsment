8217ed4baa915181910d4154a3c982cbaa328d1e
Syadon <bokagabor4@gmail.com>
2022-11-08 00:11:25 +0100

    updated page with additional className prop

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Page = ({title, children, noCard}) => {
+const Page = ({title, children, noCard, className}) => {
   return (
-    <div className="container pt-3">
+    <div className={`container pt-3`}>
       {title ? <h5>{title}</h5> : null}
-      <div className={!noCard ? "card bg-white shadow p-3" : null}>
+      <div className={!noCard ? `card bg-white shadow p-3 ${className ? className : ""}` : `${className ? className : ""}`}>
         {children}
       </div>
     </div>

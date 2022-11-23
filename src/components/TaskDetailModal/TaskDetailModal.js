0e48c274e8195e376bc295262a40957129cf2472
Syadon <bokagabor4@gmail.com>
2022-11-23 21:04:19 +0100

    refactored with new components

--- src/components/TaskDetailModal/TaskDetailModal.js
+++ src/components/TaskDetailModal/TaskDetailModal.js
@@ -1,39 +1,58 @@
-import React from 'react'
-import Button from '../Button/Button';
-//import request from '../../util/request';
-//import { useAuth } from '../../contexts/AuthContext';
+import React, {useState, useEffect, useRef} from 'react'
 
 import 'react-autocomplete-input/dist/bundle.css';
-import { getNodeAtPath } from '../../util/tree-data-utils';
+import TaskDetail from '../TaskDetail/TaskDetail';
+import TaskEdit from '../TaskEdit/TaskEdit';
 
-const TaskDetailModal = ({taskId, path, title, desc, endDate, treeData, setTreeData, removeNode, setTasksToDelete}) => {
-    const getNodeKey = ({ treeIndex }) => treeIndex;
+import request from '../../util/request';
 
-    //const {user} = useAuth();
-      // emails: emails: selectedUsers.slice(1).trim().split("@").map((act) => act.trim()+"@gmail.com")
+const TaskDetailModal = ({taskId, refresh, users, path, title, desc, subtasks, endDate, treeData, setTreeData, removeNode, setTasksToDelete, setTasksToUpdate}) => {
+
+    const [editMode, setEditMode] = useState(false);
+    const [members, setMembers] = useState([]);
+    const taskDetailModal = useRef();
+
+   useEffect(() => {
+      console.log(editMode);
+   }, [editMode]);  
+
+   useEffect(() => {
+       if(taskId) {
+        const fetchTaskMembers = async () => {
+          const res = await request.get(`/task/${taskId}/members/`);
+          if(res.data === 'Failure') {
+              setMembers([]);
+          }
+          else {
+            setMembers(res.data);
+          }
+        }
+        fetchTaskMembers();
+       }
+   }, [taskId, refresh])
+   
+   useEffect(() => {
+      let ref = null;  
+      if(taskDetailModal && taskDetailModal.current) {
+        ref = taskDetailModal.current;
+        const toggleEditMode = () => setEditMode(false);
+        ref.addEventListener('hidden.bs.modal',  toggleEditMode);  
+
+        return() => {
+            ref.removeEventListener('hidden.bs.modal', toggleEditMode);
+        }
+      }
+   }, [taskDetailModal])
 
   return (
-    <div className="modal fade" tabIndex="-1" id="taskDetailModal">
+    <div className="modal fade" tabIndex="-1" ref={taskDetailModal} id="taskDetailModal">
             <div className="modal-dialog">
                 <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">{title}</h5>
-                    <p className="modal-title" style={{position: "absolute", right: "70px"}}>{endDate}</p>
-                    <Button color="light" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></Button>
-                </div>
-                <div className="modal-body">  
-                <h6>Description:</h6>
-                <div className="mb-4">{desc}</div>    
-                </div>              
-                <div className="modal-footer">
-                    <Button color="danger" style={{position: "absolute", left: "15px"}} data-bs-dismiss="modal" onClick={() => {
-                       setTasksToDelete((prev) => [...prev, getNodeAtPath({treeData: treeData, path: path, getNodeKey: getNodeKey, ignoreCollapsed: false}).node]) 
-                       setTreeData(removeNode({treeData: treeData, path: path, getNodeKey: getNodeKey, ignoreCollapsed: false}));
-                    }                     
-                    }>Delete</Button>
-                    <Button color="secondary" data-bs-dismiss="modal">Close</Button>
-                    <Button data-bs-dismiss="modal" onClick={() => {}}>Save (in progress)</Button>
-                </div>
+                    {editMode ? 
+                    <TaskEdit taskId={taskId} users={users} members={members} prevSubtasks={subtasks} title={title} endDate={endDate} desc={desc} setTasksToDelete={setTasksToDelete} 
+                    treeData={treeData} path={path} setTreeData={setTreeData} removeNode={removeNode} setTasksToUpdate={setTasksToUpdate}/>
+                    :                      
+                    <TaskDetail title={title} subtasks={subtasks} members={members} desc={desc} endDate={endDate} setEditMode={setEditMode}/>}       
                 </div>
             </div>        
     </div>

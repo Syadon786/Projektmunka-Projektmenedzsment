6d4d392416f2b5a29a8a4b39723e1587a1fe4fc6
Syadon <bokagabor4@gmail.com>
2022-11-27 21:47:10 +0100

    added permissions, refactored node buttons

--- src/pages/ProjectTree/ProjectTree.js
+++ src/pages/ProjectTree/ProjectTree.js
@@ -17,13 +17,17 @@ import TaskModal from '../../components/TaskModal/TaskModal';
 import request from '../../util/request';
 import TaskDetailModal from '../../components/TaskDetailModal/TaskDetailModal';
 import axios from 'axios';
+import AccessDenied from '../../components/AccessDenied/AccessDenied'
 
 import { ToastContainer, toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import TaskUserModal from '../../components/TaskUserModal/TaskUserModal';
+import { useAuth } from '../../contexts/AuthContext';
 
 const ProjectTree = () => {
   const {actProject, projectTreeData, setProjectTreeData} = useProject();
+  const {user} = useAuth();
   const [treeData, setTreeData] = useState([{}]);
   const [oldTreeData, setOldTreeData] = useState([{}]);
 
@@ -35,8 +39,11 @@ const ProjectTree = () => {
   const [tasksToUpdate, setTasksToUpdate] = useState([]);
   const [actRowInfo, setActRowInfo] = useState({node: {taskId: "", title: "", description: ""}});
   const [users, setUsers] = useState([]);
+  const [permissions, setPermissions] = useState({});
   
   const [refreshModal, setRefreshModal] = useState(false);
+  const [refreshTaskMembers, setRefreshTaskMembers] = useState(false);
+  const [refreshPermissions, setRefreshPermissions] = useState(false);
 
   useEffect(() => {
     const fetchProjectUsers = async () => {
@@ -45,8 +52,20 @@ const ProjectTree = () => {
           setUsers(res.data.map((act) => act.email.split('@')[0]));
         }
     };
+   
     fetchProjectUsers();
+  
   }, [actProject.value]);  
+
+  useEffect(() => {
+    const fetchProjectPermissions = async () => {
+      const res = await request.get(`/permissions/${actProject.value}`);
+       if(res.data) {
+         setPermissions(res.data);
+      }
+    }
+    fetchProjectPermissions();
+  }, [actProject.value, refreshPermissions])
 
   const handleUpdate = async () => {
       const toDelete = [];
@@ -99,12 +118,13 @@ const ProjectTree = () => {
         }),
       ]).then(axios.spread((...response) => {
         console.log(response);
-        toast.success(`Project tree was successfully updated.`);
         setProjectTreeData(treeData);
         setNewTasks([]);
         setTasksToDelete([]);
         setTasksToUpdate([]);
         setRefreshModal(prev => !prev);
+        setRefreshTaskMembers(prev => !prev);
+        toast.success("Project Tree was successfully updated.")
       }));
   }
 
@@ -160,12 +180,25 @@ const ProjectTree = () => {
             }           
             return ({
             buttons: [
-              <button className="bar-btn" data-bs-toggle="modal" data-bs-target="#taskDetailModal" onClick={() => {                             
+              <Button color="light" className="btn-node" data-bs-toggle="modal" data-bs-target={permissions[`${rowInfo.node.taskId}`]?.[`${user.googleId}`]?.["setPermissions"] 
+              || actProject.owner === user.googleId ?
+               "#taskUserModal" : "#accessDenied"}  onClick={() => {
+                setActRowInfo(rowInfo)
+                setRefreshPermissions(prev => !prev);
+                }}>
+                  <i className="bi bi-people"></i>
+              </Button>,
+              <Button color="light" className="btn-node" data-bs-toggle="modal" 
+              data-bs-target={permissions[`${rowInfo.node.taskId}`]?.[`${user.googleId}`]?.["read"] 
+              || actProject.owner === user.googleId ?
+               "#taskDetailModal" : "#accessDenied"} onClick={() => {                             
                         setActRowInfo(rowInfo)
+                        setRefreshPermissions(prev => !prev);
                         console.log(rowInfo.node);
               }}>
-                <CircularProgressbar className='bar' value={0.66} maxValue={1} text="66%"/>
-              </button>,
+                {/* <CircularProgressbar className='bar' value={0.66} maxValue={1} text="66%"/> */}
+                <i className="bi bi-info-square"></i>
+              </Button>,
               <button className="add-btn" data-bs-toggle="modal" data-bs-target="#taskModal"
                   onClick={() => {       
                     setActRowInfo(rowInfo)
@@ -193,10 +226,13 @@ const ProjectTree = () => {
 
         <TaskModal title="Create a New Task"  users={users} treeData={treeData} rowInfo={actRowInfo} setTreeData={setTreeData} setNewTask={setNewTasks}/>
     
-        <TaskDetailModal path={actRowInfo.path} title={actRowInfo.node.title} subtasks={actRowInfo.node.subtasks} users={users} desc={actRowInfo.node.description} taskId={actRowInfo.node.taskId} 
+        <TaskDetailModal path={actRowInfo.path} newTasks={newTasks} permissions={permissions} title={actRowInfo.node.title} subtasks={actRowInfo.node.subtasks} users={users} desc={actRowInfo.node.description} taskId={actRowInfo.node.taskId} 
         endDate={actRowInfo.node.endDate} refresh={refreshModal} 
         setTreeData={setTreeData} treeData={treeData} removeNode={removeNodeAtPath} setTasksToDelete={setTasksToDelete} setTasksToUpdate={setTasksToUpdate}></TaskDetailModal>
+        <TaskUserModal projectId={actProject.value} taskTitle={actRowInfo.node.title} taskId={actRowInfo.node.taskId} refreshPermissions={setRefreshPermissions} refreshTaskMembers={refreshTaskMembers} permissions={permissions} setPermissions={setPermissions}/>
+        <AccessDenied/>
         <ToastContainer/>
+        {/* permissions[`${taskId}`]?.[`${user.googleId}`]?.["setPermissions"] || actProject.owner === user.googleId ? */}
     </Page>
   )
 

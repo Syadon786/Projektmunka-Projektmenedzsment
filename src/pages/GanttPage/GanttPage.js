b7638a6062539d20270521cf0fabb4d807e4dde8
Syadon <bokagabor4@gmail.com>
2022-11-29 15:52:21 +0100

    displaying colored tasks based on their state

--- src/pages/GanttPage/GanttPage.js
+++ src/pages/GanttPage/GanttPage.js
@@ -1,44 +1,37 @@
 import React from 'react'
-import { useEffect , useState} from 'react';
 import Page from '../../components/Page/Page';
 import { useProject } from '../../contexts/ProjectContext';
-import request from '../../util/request';
-
 import { Gantt} from 'gantt-task-react';
 
 import "gantt-task-react/dist/index.css";
+import { useEffect } from 'react';
 
 const GanttPage = () => {
-    const {actProject} = useProject();
-    const [tasks, setTasks] = useState([]);
+    const {tasks, progressMap, setRefreshProgress} = useProject();
 
     useEffect(() => {
-        const fetchTasks = async () => {
-            const res = await request.get(`${actProject.value}/task/`)
-            if(res.data !== "Failure") {
-                console.log(res.data);
-                setTasks(res.data);
-            }
-        };
-        fetchTasks();
-    }, [actProject]);
-
-    useEffect(() => {
-        console.log(tasks);
-    }, [tasks])
+        setRefreshProgress(prev => !prev);
+    }, [setRefreshProgress])
 
     return (
         <Page title="Gantt Chart">
-            { tasks.length !== 0 ? <Gantt listCellWidth='' ganttHeight="75vh" columnWidth="60" todayColor='#aab5b9' 
+            { tasks.length !== 0 && progressMap.length > 0 ? <Gantt listCellWidth='' ganttHeight="75vh" columnWidth="60" todayColor='#aab5b9' 
             barProgressColor='#3e97c7' barProgressSelectedColor='#61dafb'
             tasks={tasks.map(task => { 
-                if(new Date(task.endDate) <= new Date()) {
-                    return ({start: new Date(task.startDate), end: new Date(task.endDate), name: task.title, id: task._id, progress: 33, 
+                const progress = progressMap.find(p => p.id === task._id).progress
+                if(progress === 100) {
+                    return ({start: new Date(task.startDate), end: new Date(task.endDate), name: task.title, id: task._id, progress: progress, 
+                        isDisabled: true,
+                        type: 'task',  
+                    styles: { progressColor: '#379237', progressSelectedColor: '#54B435' }})
+                }
+                else if(new Date(task.endDate) <= new Date()) {
+                    return ({start: new Date(task.startDate), end: new Date(task.endDate), name: task.title, id: task._id, progress: progress, 
                         isDisabled: true,
                         type: 'task',  
                     styles: { progressColor: '#b82828', progressSelectedColor: '#e83333' }})
                 }
-                return ({start: new Date(task.startDate), end: new Date(task.endDate), name: task.title, id: task._id, progress: 33, 
+                return ({start: new Date(task.startDate), end: new Date(task.endDate), name: task.title, id: task._id, progress: progress, 
                 isDisabled: true,
                 type: 'task',  
                 })})}/> 
